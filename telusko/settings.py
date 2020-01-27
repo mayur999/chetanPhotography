b3d3b4c50189302e06b11630c91d281bce9f8be3
Siji Menon <siji.menon@northgateps.com>
2020-01-27 19:00:00 +0530

    bug fixes

--- telusko/settings.py
+++ telusko/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    'travello',
+    'travello.apps.TravelloConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
