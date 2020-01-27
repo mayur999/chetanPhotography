ee24b25520e4117a4318ffce6f8900834617ac0d
Siji Menon <siji.menon@northgateps.com>
2020-01-27 18:12:29 +0530

    module name change

--- AwsDemo/TestProject/settings.py
+++ AwsDemo/TestProject/settings.py
@@ -78,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'postgres',
-        'USER': 'django',
-        'PASSWORD': 'django',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
