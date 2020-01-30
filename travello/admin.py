d103c5a9d33a21b46f96f338a46d8abead3be3a0
Siji Menon <siji.menon@northgateps.com>
2020-01-30 19:57:29 +0530

    project deleted

--- travello/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Destination
-from .models import Photo
-
-# Register your models here.
-admin.site.register(Destination)
-admin.site.register(Photo)
